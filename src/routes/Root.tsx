b1c98d7a5de32621d8a6f6a12045392ca00f2964
John Costanzo <jrock2004@gmail.com>
2026-02-27 16:14:00 -0500

    fixing scrolling

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from 'react'
-import { Outlet } from 'react-router'
+import { Outlet, ScrollRestoration } from 'react-router'
 import { Header } from '@components/Header'
 import { NavBar } from '@components/NavBar'
 import { Footer } from '@components/Footer'
@@ -13,6 +13,7 @@ const Root = (): ReactElement => {
         <Outlet />
       </main>
       <Footer />
+      <ScrollRestoration />
     </div>
   )
 }
